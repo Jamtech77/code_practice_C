09b176e91d075283c2d6ab69b57b3a057fa7c94f
james.cyhuang <james.cyhuang@mic.com.tw>
2020-10-20 18:18:10 +0800

    Update Validate_Parenthese.c(Not finish yet)

--- Validate_Parentheses.c
+++ Validate_Parentheses.c
@@ -1,4 +1,5 @@
 #include "stdio.h"
+#include <stdlib.h>
 
 #define CIR_BRACKET_L '('
 #define CIR_BRACKET_R ')'
@@ -7,21 +8,42 @@
 #define SQUARE_BRACKET_L '['
 #define SQUARE_BRACKET_R ']'
 
+int mapping_bracket(char a, char b){  // True or False
+    if(a == CIR_BRACKET_L){
+        if(b == CIR_BRACKET_R) return 1;
+    } else if (a == CURLY_BRACKET_L){
+        if(b == CURLY_BRACKET_R) return 1;
+    } else if (a == SQUARE_BRACKET_L){
+        if(b == SQUARE_BRACKET_R) return 1;
+    } else {
+        return 0;
+    }
+}
 
 int isValid(char *str){
     int ret = 0;
     int len = 0;
+    int index = 0;
     
     while(*(str + len) != '\0'){
         len++;
     }
     printf("%d\n", len);
     
-    for (int i = 0; i < len; i++) {
-      printf("%d\n", str[i]);
+    char *stack = (char *)malloc(sizeof(char) * len);
+    
+    for(int i = 0; i < len; i++){
+        if( mapping_bracket(str[i], stack[index]) ){
+            // stack_pop()
+            stack[index] = '\0';
+            index -= 1;
+            if(index <= 0) index = 0;
+        } else {
+            // Stack_Append
+        }
     }
     
-    
+    // Is stack empty?
     
     return ret;
 }
@@ -41,13 +63,15 @@ int main(void) {
     char *str2 = "[]{}()";  //True
     char *str3 = "[()]{}";  //True
     char *str4 = "({[)]";  //False
-    char *str2 = "[{]}()";  //False
+    char *str5 = "[{]}()";  //False
+    char *str6 = "[[[[[]]";  //True
     
-    isValid("()[]{}");
-    
-    printf("%d\n", '('^')');
-    printf("%d\n", '['^']');
-    printf("%d\n", '{'^'}');
+    // isValid("()[]{}");
+    // 
+    // // printf("%d\n", '('^')');
+    // // printf("%d\n", '['^']');
+    // // printf("%d\n", '{'^'}');
+
     
     return 0;
 }
